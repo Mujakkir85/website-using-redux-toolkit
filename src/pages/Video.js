6be4389cb611661c786e32bdc095506533859da9
Mujakkir85 <mujakkirlucse@gmail.com>
2022-10-17 22:25:40 +0600

    signle video done

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,25 +1,54 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import VideoPlayer from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
-import Footer from "../components/Footer";
 import RelatedVideoList from "../components/list/RelatedVideoList";
-import Navbar from "../components/navbar/Navbar";
+import { fetchVideo } from "../features/video/videoSlice";
+import Loading from "../components/ui/Loading";
 
 export default function Video() {
-    return (
-        <>
-            <section class="pt-6 pb-20">
-                <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                    <div class="grid grid-cols-3 gap-2 lg:gap-8">
-                        <div class="col-span-full w-full space-y-8 lg:col-span-2">
-                            <VideoPlayer />
+  const { video, isLoading, isError, error } = useSelector(
+    (state) => state.video
+  );
+  const dispatch = useDispatch();
+  const { videoId } = useParams();
 
-                            <VideoDescription />
-                        </div>
+  useEffect(() => {
+    dispatch(fetchVideo(videoId));
+  }, []);
 
-                        <RelatedVideoList />
-                    </div>
-                </div>
-            </section>
-        </>
+  const { title, link, id, tags } = video || {};
+
+  let content = null;
+
+  if (isLoading) content = <Loading />;
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+
+  if (!isLoading && !isError && !video?.id) {
+    content = <div className="col-span-12">No Video found</div>;
+  }
+
+  if (!isLoading && !isError && video?.id) {
+    content = (
+      <div class="grid grid-cols-3 gap-2 lg:gap-8">
+        <div class="col-span-full w-full space-y-8 lg:col-span-2">
+          <VideoPlayer title={title} link={link} />
+
+          <VideoDescription video={video} />
+        </div>
+
+        <RelatedVideoList currentVideoId={id} tags={tags} />
+      </div>
     );
+  }
+
+  return (
+    <>
+      <section class="pt-6 pb-20">
+        <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">{content}</div>
+      </section>
+    </>
+  );
 }
